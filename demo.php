11d2f0443ddb34e4aa1c18c13fcf7455c5d1ee55
uttamrakholiya <36851474+uttamrakholiya@users.noreply.github.com>
2018-09-28 13:15:43 +0530

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,33 @@
+<span style="color: rgb(84, 84, 84); font-family: arial, sans-serif; font-size: small;">
+	Note that if you only&nbsp;
+</span>
+<span style="font-weight: bold; color: rgb(106, 106, 106); font-family: arial, sans-serif; font-size: small;">
+	allow
+</span>
+<span style="color: rgb(84, 84, 84); font-family: arial, sans-serif; font-size: small;">
+	&nbsp;the '
+</span>
+<span style="font-weight: bold; color: rgb(106, 106, 106); font-family: arial, sans-serif; font-size: small;">
+	text
+</span>
+<span style="color: rgb(84, 84, 84); font-family: arial, sans-serif; font-size: small;">' 
+	context, you will still be able to&nbsp;
+</span>
+<span style="font-weight: bold; color: rgb(106, 106, 106); font-family: arial, sans-serif; font-size: small;">
+	copy text
+</span>
+<span style="color: rgb(84, 84, 84); font-family: arial, sans-serif; font-size: small;">
+	&nbsp;... used by&nbsp;
+</span>
+<span style="font-weight: bold; color: rgb(106, 106, 106); font-family: arial, sans-serif; font-size: small;">
+	Copy
+</span>
+<span style="color: rgb(84, 84, 84); font-family: arial, sans-serif; font-size: small;">
+	Formatting can be done by adding custom styles to the&nbsp;
+</span>
+<span style="font-weight: bold; color: rgb(106, 106, 106); font-family: arial, sans-serif; font-size: small;">
+	editor
+</span>
+<span style="color: rgb(84, 84, 84); font-family: arial, sans-serif; font-size: small;">
+	&nbsp;...
+</span>
